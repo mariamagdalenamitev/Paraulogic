cd96a4ca0665ef62f7487bda009fa9a36d8cee28
mariamagdalena <mariamagdalenamitev@alumnat.iesdamiahuguet.net>
2024-01-12 08:53:45 +0100

    Commit 2n dia del paraulogic

---
+++ paraulogic.js
@@ -0,0 +1,13 @@
+let paraula
+function llegirParaula(){
+    document.getElementById()
+}
+
+let paraula
+let lletra
+
+function comprovarLletra(paraula, lletra){
+    document.getElementById(paraula.includes(lletra)).innerHTML
+}
+
+//Si per exemple una paraula té tres "a" i una està ben col·locada però les altres dues no, aquestes dues no se pinten de groc
