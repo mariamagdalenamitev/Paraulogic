f69d9d17e7e2b45cf9c7a854cd2659ca87a07943
mariamagdalena <mariamagdalenamitev@alumnat.iesdamiahuguet.net>
2024-01-19 08:50:52 +0100

    Commit 5è dia del paraulògic

--- paraulogic.js
+++ paraulogic.js
@@ -8,11 +8,30 @@ console.log(paraula)
         window.alert('Has guanyat!')
         document.getElementById()
     }
-    //(paraula.length===secreta.lenght)
+let secreta= 'i';
+
+novaEntrada= ' ';
+novaEntrada=<div class="slot"></div>
+
+    if (paraula.length===secreta.lenght){
+        for (let i = 0; i<5; i++){
+            document.getElementById('solucions')
+            //fer un chartAt per cada lletra (lletra per lletra) i després afegir-li la nova entrada
+        }
+    }else{
+        window.alert('La paraula no té la mida correcta')
+        document.getElementById()
+    }
 
     /*function comprovarLletra(paraula, lletra){
     document.getElementById(paraula.includes(lletra)).innerHTML*/
 }
 
 //Si per exemple una paraula té tres "a" i una està ben col·locada però les altres dues no, aquestes dues no se pinten de groc
+//Fer un for per pintar les lletres amb "div"
 
+/*function posicioCaracter(paraula,lletra){
+    console.log(paraula.charAt(lletra))
+}
+posicioCaracter('Jaume',2); //u
+posicioCaracter('Ensaïmada',5) //m */
