66cc9dee991ed286714d16a60e3d30d105f1f48c
mariamagdalena <mariamagdalenamitev@alumnat.iesdamiahuguet.net>
2024-01-26 08:53:32 +0100

    Commit sessió 8

--- paraulogic.js
+++ paraulogic.js
@@ -4,12 +4,7 @@ function llegirParaula() {
     let paraula = document.getElementById('resposta').value.toUpperCase()
     console.log(paraula);
 
-    if (paraula === secreta) {
-        window.alert('Has guanyat!');
-        //TODO programar que primer pinti la paraula i després posi el missatge
-    }
-
-    novaEntrada = '';
+    novaEntrada = '<div class="resposta" >';
     if (paraula.length === secreta.length) {
         for (let i = 0; i < 5; i++) {
             if (secreta.charAt(i) === paraula.charAt(i)) {
@@ -26,23 +21,20 @@ function llegirParaula() {
                 novaEntrada += '</div>'
             }
         }
-        document.getElementById('solucions').innerHTML = novaEntrada;
+        if (paraula === secreta) {
+            window.alert('Has guanyat!');
+        }
+        novaEntrada+="</div>"
+        document.getElementById('solucions').innerHTML += novaEntrada;
     } else {
         window.alert('La paraula no té la mida correcta');
     }
 }
 
-/*function keyboard() {
-    lletraQ = Q  lletraW = W  lletraE = E  lletraR = R  lletraT = T  lletraY = Y  lletraU = U  lletraI = I
-    lletraO = O  lletraP = P  lletraA = A  lletraS = S  lletraD = D  lletraF = F  lletraG = G  lletraH = H
-    lletraJ = J  lletraK = K  lletraL = L  lletraÑ = Ñ  lletraÇ = Ç  lletraZ = Z  lletraX = X  lletraC = C
-    lletraV = V  lletraB = B  lletraN = N  lletraM = M
+function imatgePista(){
+    window.alert('La paraula té ' + secreta.length + ' lletres i lhas descriure en català');
+}
 
-    document.getElementById('lletres').innerHTML = teclat
-}*/
-    /*function comprovarLletra(paraula, lletra){
-    document.getElementById(paraula.includes(lletra)).innerHTML*/
-//TODO només necessit una a, que no me pinti les demes
-//TODO que no substituesqui la paraula escrita, que fassi una fila nova
+//TODO només necessit una a, que no me pinti les demes(Només si me sobra temps)
 //TODO programar tecla intro
 //TODO programar que me pinti les lletres del teclat a dins el requadre de la paraula, i a poder ser que me pinti les lletres
